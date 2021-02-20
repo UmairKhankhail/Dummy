446494cadfdadc0e984bd407cd1f245b497b6fa3
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-02-20 19:31:46 +0500

    added a print function
    added a new print function in the existing funciton

--- Dummy project/functions.py
+++ Dummy project/functions.py
@@ -3,3 +3,4 @@ def calculateAreaOfSquare(width,length):
 width=int(input("Enter the width of square: "))
 length=int(input("Enter the length of square: "))
 print("The Area of Square is : ",calculateAreaOfSquare(width,length))
+print("I have changed this function to commit for Branch-1")
