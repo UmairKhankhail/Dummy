e32d75101a38d8d31c36dc8fd7640499402dab83
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-02-20 18:35:17 +0500

    changed the complete code
    Changed the complete code.

--- Dummy project/functions.py
+++ Dummy project/functions.py
@@ -1,17 +1,5 @@
-import math
-def cube(x):
-    return x**3
-
-def sqrt(y):
-    return math.sqrt(y)
-
-def greet(my_name):
-    return "Have a great day : "+my_name
-print("this statement comes in main and it always will run when ever we import a file.")
-
-#if we have to find where the line is being executed then
-#now if i say if this function is directly called then run the below lines then we use if conditon
-if __name__=="__main__":
- print("This the library which we imported in online lecture no 03: "+__name__)
-else:
-    print("Function"+__name__+"has been called.")
+def calculateAreaOfSquare(width,length):
+     return width*length
+width=int(input("Enter the width of square: "))
+length=int(input("Enter the length of square: "))
+print("The Area of Square is : ",calculateAreaOfSquare(width,length))
